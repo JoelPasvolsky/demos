2f03585c3d1eb0474f4995c339cd3bbbd21bdf44
Joel Pasvolsky <jpasvolsky@dwavesys.com>
2019-04-12 10:26:10 -0700

    Change to pandas DataFrame

--- examples/miqubo/helpers/plots.py
+++ examples/miqubo/helpers/plots.py
@@ -16,25 +16,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.gridspec import GridSpec
 
-def subplot_x_1(x, y):
-    gs = GridSpec(2, 3)
+def subplot_xx_y(df):
+    xnum = df.shape[1]-1
+    gs = GridSpec(2, xnum)
     plt.figure(figsize=(10, 10))
-    for i in range(len(x)):
-        xi = x[i]
-        ax = 'ax' + str(xi)
+    for i in range(xnum):
+        ax = 'ax_' + df.columns[i]
         ax = plt.subplot(gs[0, i])
-        ax.set_title("Input "+str(i))
-        ax.plot(np.linspace(-np.pi, np.pi, 100), xi)
+        ax.set_title(df.columns[i])
+        ax.plot(np.linspace(-np.pi, np.pi, 100), df[df.columns[i]].values)
     axy = plt.subplot(gs[1, :])
-    axy.set_title("Output")
-    axy.plot(np.linspace(-np.pi, np.pi, 100), y, 'r')
+    axy.set_title(df.columns[-1])
+    axy.plot(np.linspace(-np.pi, np.pi, 100), df[df.columns[-1]].values, 'r')
 
-def plot_12_13(x, y, model, popt_12, popt_13):
+def plot_vars_out(data, vars1, vars2, y, model):
     ax1 = plt.subplot(1, 2, 1)
-    ax1.set_title("f(in1, in2)")
+    ax1.set_title("Modeling %s" % vars1[:2])
     ax1.plot(np.linspace(-np.pi, np.pi, 100), y, 'r')
-    ax1.plot(np.linspace(-np.pi, np.pi, 100), model((x[0], x[1]), *popt_12), 'b.')
+    ax1.plot(np.linspace(-np.pi, np.pi, 100), model((data[vars1[0]].values, data[vars1[1]].values), *vars1[2]), 'b.')
+    ax1.legend(["out", "model"])
     ax2 = plt.subplot(1, 2, 2)
-    ax2.set_title("f(in1, in3)")
+    ax2.set_title("Modeling %s" % vars2[:2])
     ax2.plot(np.linspace(-np.pi, np.pi, 100), y, 'r')
-    ax2.plot(np.linspace(-np.pi, np.pi, 100), model((x[0], x[2]), *popt_13), 'b.')
+    ax2.plot(np.linspace(-np.pi, np.pi, 100), model((data[vars2[0]].values, data[vars2[1]].values), *vars2[2]), 'b.')
+    ax1.legend(["out", "model"])
